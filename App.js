db92dfcbae33d14077a4e5829dfea1c5842ea8b6
ozaki <fs65ugumori@yahoo.co.jp>
2018-08-23 00:07:13 +0900

    add button

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Button, Linking, Text, View } from 'react-native';
 import fetch from 'react-native-fetch-polyfill';
+
+const url = 'https://syoto-test.herokuapp.com/test';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -8,21 +10,25 @@ export default class App extends React.Component {
     this.state = { agent: '-' };
   }
 
-  async componentWillMount() {
+  getUserAgent = async () => {
     try {
-      const res = await fetch('https://syoto-test.herokuapp.com/test');
+      const res = await fetch(url);
       console.log(res);
       const { agent } = await res.json();
       this.setState({ agent });
     } catch (e) {
       this.setState({ agent: 'ERROR' });
     }
-  }
+  };
+
+  openBrowser = () => Linking.openURL(url);
 
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text style={{ fontSize: 24 }}>{this.state.agent}</Text>
+        <Button title="UserAgentを取得" onPress={this.getUserAgent} />
+        <Button title="ブラウザで開く" onPress={this.openBrowser} />
       </View>
     );
   }
