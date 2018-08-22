ebb7d1d3df3fdc84eb56f91a90210083433ef959
ozaki25 <fs65ugumori@yahoo.co.jp>
2018-08-22 13:45:25 +0900

    add communication to App.js

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Platform, Text, View } from 'react-native';
+import fetch from 'react-native-fetch-polyfill';
+
+const host = Platform.select({
+  android: '10.0.2.2',
+  ios: 'localhost',
+});
+
+const port = '3000';
 
 export default class App extends React.Component {
+  componentWillMount() {
+    const url = `http://${host}:${port}/test`;
+    fetch(url);
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+      <View>
+        <Text>Welcome to React Native!</Text>
       </View>
     );
   }
